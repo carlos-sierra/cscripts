8fa475d18ae829660e310ebd5c9971857cadea40
Carlos Sierra <carlos.sierra.usa@gmail.com>
2017-06-11 20:17:13 -0700

    v1714

--- sql/planx.sql
+++ sql/planx.sql
@@ -259,6 +259,70 @@ SELECT   inst_id
        , child_number
 /
 
+PRO
+PRO GV$SQL (ordered by et_secs_per_exec)
+PRO ~~~~~~
+SELECT   plan_hash_value
+       , TO_CHAR(ROUND(SUM(elapsed_time)/SUM(executions)/1e6,6), '999,990.000000') et_secs_per_exec
+       , TO_CHAR(ROUND(SUM(cpu_time)/SUM(executions)/1e6,6), '999,990.000000') cpu_secs_per_exec
+       , SUM(executions) executions
+       , TO_CHAR(ROUND(SUM(elapsed_time)/1e6,6), '999,999,999,990') et_secs_tot
+       , TO_CHAR(ROUND(SUM(cpu_time)/1e6,6), '999,999,999,990') cpu_secs_tot
+       , COUNT(DISTINCT child_number) cursors
+       , MAX(child_number) max_child
+       , SUM(CASE is_shareable WHEN 'Y' THEN 1 ELSE 0 END) shareable
+       , TO_CHAR(MAX(last_active_time), 'YYYY-MM-DD"T"HH24:MI:SS') last_active_time
+       , ROUND(SUM(buffer_gets)/SUM(executions)) buffers_per_exec
+       , TO_CHAR(ROUND(SUM(rows_processed)/SUM(executions), 3), '999,999,999,990.000') rows_per_exec
+  FROM gv$sql
+ WHERE sql_id = :sql_id
+   AND executions > 0
+ GROUP BY
+       plan_hash_value
+ ORDER BY
+       2
+/
+
+PRO
+PRO GV$SQL (ordered by inst_id and child_number)
+PRO ~~~~~~
+SELECT   inst_id
+       , child_number
+       , plan_hash_value
+       , is_shareable s
+       , TO_CHAR(ROUND(elapsed_time/executions/1e6,6), '999,990.000000') et_secs_per_exec
+       , TO_CHAR(ROUND(cpu_time/executions/1e6,6), '999,990.000000') cpu_secs_per_exec
+       , executions
+       , TO_CHAR(ROUND(elapsed_time/1e6,6), '999,999,999,990') et_secs_tot
+       , TO_CHAR(ROUND(cpu_time/1e6,6), '999,999,999,990') cpu_secs_tot
+       , TO_CHAR(last_active_time, 'YYYY-MM-DD"T"HH24:MI:SS') last_active_time
+       , ROUND(buffer_gets/executions) buffers_per_exec
+       , TO_CHAR(ROUND(rows_processed/executions, 3), '999,999,999,990.000') rows_per_exec
+  FROM gv$sql
+ WHERE sql_id = :sql_id
+   AND executions > 0
+ ORDER BY
+       inst_id,
+       child_number
+/
+
+PRO
+PRO GV$SQL (ordered by inst_id and child_number)
+PRO ~~~~~~
+SELECT   inst_id
+       , child_number
+       , plan_hash_value
+       , sql_profile
+       &&is_10g., sql_plan_baseline
+       &&is_10g., sql_patch
+  FROM gv$sql
+ WHERE sql_id = :sql_id
+   AND executions > 0
+ ORDER BY
+       inst_id,
+       child_number
+/
+
 PRO       
 PRO GV$SQL_PLAN_STATISTICS_ALL LAST (ordered by inst_id and child_number)
 PRO ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -380,6 +444,26 @@ SELECT   s.snap_id
    AND s.dbid = h.dbid
    AND s.instance_number = h.instance_number
  ORDER BY 1 DESC, 4, 5
+/
+
+PRO
+PRO DBA_HIST_SQLSTAT DELTA (ordered by et_secs_per_exec)
+PRO ~~~~~~~~~~~~~~~~~~~~~~
+SELECT   plan_hash_value
+       , TO_CHAR(ROUND(SUM(elapsed_time_delta)/SUM(executions_delta)/1e6,6), '999,990.000000') et_secs_per_exec
+       , TO_CHAR(ROUND(SUM(cpu_time_delta)/SUM(executions_delta)/1e6,6), '999,990.000000') cpu_secs_per_exec
+       , SUM(executions_delta) executions
+       , TO_CHAR(ROUND(SUM(elapsed_time_delta)/1e6,6), '999,999,999,990') et_secs_tot
+       , TO_CHAR(ROUND(SUM(cpu_time_delta)/1e6,6), '999,999,999,990') cpu_secs_tot
+       , ROUND(SUM(buffer_gets_delta)/SUM(executions_delta)) buffers_per_exec
+       , TO_CHAR(ROUND(SUM(rows_processed_delta)/SUM(executions_delta), 3), '999,999,999,990.000') rows_per_exec
+  FROM dba_hist_sqlstat
+ WHERE sql_id = :sql_id
+   AND executions_delta > 0
+ GROUP BY
+       plan_hash_value
+ ORDER BY
+       2
 /
 
 PRO
--- sql/spm/create_spb_from_awr.sql
+++ sql/spm/create_spb_from_awr.sql
@@ -1,40 +1,39 @@
-REM $Header: 215187.1 create_spb_from_awr.sql 11.4.5.8 2013/05/10 carlos.sierra $
-
-PAU Requires Oracle Tuning Pack license. Hit "Enter" to proceed
-
-ACC sql_text_piece PROMPT 'Enter SQL Text piece: '
-
-SET PAGES 200 LONG 80000 ECHO ON;
-
-COL sql_text PRI;
-
-SELECT dbid, sql_id, sql_text /* exclude_me */
-  FROM dba_hist_sqltext
- WHERE sql_text LIKE '%&&sql_text_piece.%'
-   AND sql_text NOT LIKE '%/* exclude_me */%';
-
-ACC dbid NUM PROMPT 'Enter DBID: ';
+SET PAGES 200 LONG 80000;
 
 ACC sql_id PROMPT 'Enter SQL_ID: ';
 
-SELECT p.snap_id, s.startup_time, s.begin_interval_time, s.end_interval_time, /* exclude_me */
-       p.instance_number, p.plan_hash_value, p.executions_total, p.elapsed_time_total,
-       CASE WHEN p.executions_total > 0 THEN ROUND(p.elapsed_time_total/p.executions_total/1e6, 3) END avg_secs_per_exec
-  FROM dba_hist_sqlstat p,
-       dba_hist_snapshot s
- WHERE p.dbid = &&dbid
-   AND p.sql_id = '&&sql_id.'
-   AND s.snap_id = p.snap_id
-   AND s.dbid = p.dbid
-   AND s.instance_number = p.instance_number
+WITH
+p AS (
+SELECT plan_hash_value
+  FROM dba_hist_sql_plan
+ WHERE sql_id = TRIM('&&sql_id.')
+   AND other_xml IS NOT NULL ),
+a AS (
+SELECT plan_hash_value,
+       SUM(elapsed_time_total)/SUM(executions_total) avg_et_secs,
+       MAX(executions_total) executions_total
+  FROM dba_hist_sqlstat
+ WHERE sql_id = TRIM('&&sql_id.')
+   AND executions_total > 0
+ GROUP BY
+       plan_hash_value )
+SELECT p.plan_hash_value,
+       ROUND(a.avg_et_secs/1e6, 6) avg_et_secs,
+       a.executions_total
+  FROM p, a
+ WHERE p.plan_hash_value = a.plan_hash_value(+)
  ORDER BY
-       p.snap_id, p.plan_hash_value;
+       avg_et_secs NULLS LAST;
 
 ACC plan_hash_value PROMPT 'Enter Plan Hash Value: ';
 
-SELECT p.snap_id, s.startup_time, s.begin_interval_time, s.end_interval_time, /* exclude_me */
-       p.instance_number, p.executions_total, p.elapsed_time_total,
-       CASE WHEN p.executions_total > 0 THEN ROUND(p.elapsed_time_total/p.executions_total/1e6, 3) END avg_secs_per_exec
+COL dbid NEW_V dbid NOPRI;
+SELECT dbid FROM v$database;
+
+COL begin_snap_id NEW_V begin_snap_id NOPRI;
+COL end_snap_id NEW_V end_snap_id NOPRI;
+
+SELECT MIN(p.snap_id) begin_snap_id, MAX(p.snap_id) end_snap_id
   FROM dba_hist_sqlstat p,
        dba_hist_snapshot s
  WHERE p.dbid = &&dbid
@@ -42,13 +41,7 @@ SELECT p.snap_id, s.startup_time, s.begin_interval_time, s.end_interval_time, /*
    AND p.plan_hash_value = TO_NUMBER('&&plan_hash_value.')
    AND s.snap_id = p.snap_id
    AND s.dbid = p.dbid
-   AND s.instance_number = p.instance_number
- ORDER BY
-       p.snap_id, p.plan_hash_value;
-
-ACC begin_snap_id NUM PROMPT 'Enter begin SNAP_ID: ';
-
-ACC end_snap_id NUM PROMPT 'Enter end SNAP_ID: ';
+   AND s.instance_number = p.instance_number;
 
 VAR sqlset_name VARCHAR2(30);
 
@@ -109,4 +102,5 @@ PRINT plans;
 
 SET PAGES 14 LONG 80 ECHO OFF SERVEROUT OFF;
 
-UNDEF sql_text_piece dbid sql_id plan_hash_value begin_snap_id end_snap_id
+UNDEF sql_id plan_hash_value
+CL COL
